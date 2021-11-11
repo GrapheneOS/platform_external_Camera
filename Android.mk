10fb88905606d94d19aa678ddb7613b56166cdda
Daniel Micay <danielmicay@gmail.com>
2021-11-11 16:15:20 -0500

    add missing uses-library build system setup

--- Android.mk
+++ Android.mk
@@ -7,5 +7,6 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_PRODUCT_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := prebuilt/Camera.apk
+LOCAL_OPTIONAL_USES_LIBRARIES := androidx.camera.extensions.impl
 
 include $(BUILD_PREBUILT)
